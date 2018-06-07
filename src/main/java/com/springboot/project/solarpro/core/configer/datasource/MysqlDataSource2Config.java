df098f49c38ccd3fa25d64a7e7d9f9afa0db43d4
lishuying <lishuying@jd.com>
2018-06-07 08:41:18 +0800

    多数据源配置

---
+++ src/main/java/com/springboot/project/solarpro/core/configer/datasource/MysqlDataSource2Config.java
@@ -0,0 +1,33 @@
+package com.springboot.project.solarpro.core.configer.datasource;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import tk.mybatis.spring.annotation.MapperScan;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = {"com.springboot.project.solarpro.dao.db2"}, sqlSessionFactoryRef = "secondSqlSessionFactory")
+public class MysqlDataSource2Config {
+
+    @Bean(name = "secondDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.db2")
+    public DataSource dataSource() {
+        return new DruidDataSource();
+    }
+
+    @Bean(name = "secondSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        Resource[] mapperLocations = new PathMatchingResourcePatternResolver().getResources("classpath:mapper/db2/*.xml");
+        sessionFactory.setMapperLocations(mapperLocations);
+        return sessionFactory.getObject();
+    }
+}
